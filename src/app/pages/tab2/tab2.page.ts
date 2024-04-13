9395ad5ae096ea63b198b189ef16cf21a7887e39
Abdiel <abdiel2012p@gmail.com>
2024-04-12 18:56:57 -0600

    InfiniteScroll added to the groups section

--- src/app/pages/tab2/tab2.page.ts
+++ src/app/pages/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonInfiniteScroll } from '@ionic/angular';
 import { Article } from 'src/app/interfaces';
 import { NewsService } from 'src/app/services/news.service';
 
@@ -8,6 +9,8 @@ import { NewsService } from 'src/app/services/news.service';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page implements OnInit{
+
+  @ViewChild(IonInfiniteScroll,{static: true}) infiniteScroll!: IonInfiniteScroll;
 
   public categories: string[] = ['business','entertainment','general','health','science','sports','technology'];
   public selectedCategory: string = this.categories[0];
@@ -19,7 +22,7 @@ export class Tab2Page implements OnInit{
     this.newsService.getTopHeadLinesByCategory(this.selectedCategory)
     .subscribe(articles => {
       // console.log(articles);
-      this.articles = [...this.articles]
+      this.articles = [...articles]
     });
   }
 
@@ -30,6 +33,23 @@ export class Tab2Page implements OnInit{
       // console.log(articles);
       this.articles = [...articles]
     });
+  }
+
+  loadData(){
+    this.newsService.getTopHeadLinesByCategory(this.selectedCategory, true)
+    .subscribe( (articles) => {
+
+      if(articles.length === this.articles.length){
+        this.infiniteScroll.disabled = true;
+        // event?.target.complete();
+        return;
+      }
+
+      this.articles = articles;
+      this.infiniteScroll.complete();
+      // event?.target.complete();
+
+    })
   }
 
 }
